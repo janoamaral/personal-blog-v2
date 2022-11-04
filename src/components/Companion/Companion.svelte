1f6e30825930a8989d4ed1b9b75960af5f709a5b
Alejandro Amaral <aamaral@logico.com.ar>
2022-11-03 21:25:06 -0300

    Improved companion style and functionality

--- src/components/Companion/Companion.svelte
+++ src/components/Companion/Companion.svelte
@@ -1,31 +1,22 @@
 <script>
   import Icon from '../Home/Icons.svelte';
-
-  let isCollapsed = false;
+  import { isCollapsed } from '../../stores/common.js';
 
   function toggleCollapse() {
-    isCollapsed = !isCollapsed;
+    $isCollapsed = !$isCollapsed;
   }
 </script>
 
-<div class="flex flex-row fixed z-50 right-0 transition-all" style="top:40vh">
+<div
+  class={`flex flex-row fixed z-50 transition-all ${$isCollapsed ? '-right-7' : 'right-0'}`}
+  style={`top:calc(35vh - ${$isCollapsed ? '0em' : '0em'})`}
+>
   <ul
     class="bg-light-black/70 backdrop-blur-sm border border-light-black text-dim-white p-1
     transition-all rounded-l-lg m-0 shadow-sm"
   >
-    <li class={`${isCollapsed ? 'h-0 overflow-hidden' : 'block'}`}>
-      <a href="/" class="hover:text-white">
-        <Icon size={6} type="home" gradientFrom="173, 182, 255, 0" gradientTo="174, 109, 255, 0" />
-      </a>
-    </li>
-    <li class={`${isCollapsed ? 'h-0 overflow-hidden' : 'block'}`}>
-      <a href="/blog?page=1" class="hover:text-white">
-        <Icon size={6} type="edit" gradientFrom="173, 182, 255, 0" gradientTo="174, 109, 255, 0" />
-      </a>
-    </li>
-
-    <li on:click={toggleCollapse} class="cursor-pointer">
-      {#if isCollapsed}
+    <li on:click={toggleCollapse} class="cursor-pointer text-light-blue">
+      {#if $isCollapsed}
         <Icon
           size={6}
           type="expand"
@@ -40,6 +31,16 @@
           gradientTo="174, 109, 255, 0"
         />
       {/if}
+    </li>
+    <li class={`${$isCollapsed ? 'h-0 overflow-hidden' : 'h-auto'}`}>
+      <a href="/" class="hover:text-white">
+        <Icon size={6} type="home" gradientFrom="173, 182, 255, 0" gradientTo="174, 109, 255, 0" />
+      </a>
+    </li>
+    <li class={`${$isCollapsed ? 'h-0 overflow-hidden' : 'h-auto'}`}>
+      <a href="/blog?page=1" class="hover:text-white">
+        <Icon size={6} type="edit" gradientFrom="173, 182, 255, 0" gradientTo="174, 109, 255, 0" />
+      </a>
     </li>
   </ul>
 </div>
