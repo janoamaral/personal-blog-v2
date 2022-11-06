e1056e57ce49e432994f7ba964e1506ca814638a
Alejandro Amaral <aamaral@logico.com.ar>
2022-11-05 22:24:45 -0300

    Added PageSpeed Insights recommendations

--- src/components/Home/AboutMe.svelte
+++ src/components/Home/AboutMe.svelte
@@ -26,14 +26,14 @@
 <div class="mb-8 text-md pt-2 rounded-lg" id="about-section">
   <div class="text-center mb-10 text-black">
     <Icon type="user" gradientFrom="76, 252, 255" gradientTo="76, 160, 255" />
-    <h3 class="font-bold text-3xl mb-2 mt-2 md:text-4xl text-white">
+    <h2 class="font-bold text-3xl mb-2 mt-2 md:text-4xl text-white">
       Know me a
       <span
         style="background:linear-gradient(122.25deg, #4CFCFF 12.16%, #4C97FF 70.98%);-webkit-background-clip:text;-webkit-text-fill-color:transparent;"
       >
         little better.
       </span>
-    </h3>
+    </h2>
   </div>
   <p class="mb-4">
     I'm a systems analyst based in Bolívar, a beautiful small city in Buenos Aires - Argentina. I
@@ -44,9 +44,9 @@
   <p class="mb-8">
     But not everything in me is about job: I'm a minimalist at heart, open source lover and bad
     harmonica player. I enjoy cooking for my family and friends, being italian pastas my forte. Avid
-    <a class="text-light-blue underline" href="/blog/1/biblioteca">reader</a> and with a very ecletic taste in music:
-    from classical music to K/J-Pop and everything in between. Nature conscious: I don't own a car and
-    prefer walk / cycling, avoid plastics everytime I can.
+    <a class="text-light-blue underline" href="/blog/1/biblioteca">reader</a> and with a very ecletic
+    taste in music: from classical music to K/J-Pop and everything in between. Nature conscious: I don't
+    own a car and prefer walk / cycling, avoid plastics everytime I can.
   </p>
   <h4 class="font-bold text-2xl mb-2 md:text-2xl">Random fact about me.</h4>
   {#if fetchFail}
