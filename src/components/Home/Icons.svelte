8074405fd3e911fcbbb978b9c9d754adf98126e4
Alejandro Amaral <aamaral@logico.com.ar>
2022-11-13 21:07:13 -0300

    Drawer working

--- src/components/Home/Icons.svelte
+++ src/components/Home/Icons.svelte
@@ -151,7 +151,7 @@
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      stroke-width="1.5"
+      stroke-width="2.2"
       stroke="currentColor"
       class="w-6 h-6"
     >
@@ -159,6 +159,23 @@
         stroke-linecap="round"
         stroke-linejoin="round"
         d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z"
+      />
+    </svg>
+  {/if}
+
+  {#if type === 'search'}
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="2.5"
+      stroke="currentColor"
+      class="w-6 h-6"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M3.75 12h16.5m-16.5 3.75h16.5M3.75 19.5h16.5M5.625 4.5h12.75a1.875 1.875 0 010 3.75H5.625a1.875 1.875 0 010-3.75z"
       />
     </svg>
   {/if}
--- src/stores/common.js
+++ src/stores/common.js
@@ -1,3 +1,5 @@
 import { writable } from 'svelte/store';
 
 export const isCollapsed = writable(false);
+
+export const isSpotlightOpen = writable(false)
