7c4a1da3de9b362a564919c7cca82f04faf08d94
Alejandro Amaral <aamaral@logico.com.ar>
2022-11-13 16:30:20 -0800

    Feature fully working (#40)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,30 +1,47 @@
 <script>
+  import { onMount } from 'svelte';
+
   import '../app.css';
   import SvelteSpotlight from 'svelte-spotlight/SvelteSpotlight.svelte';
 
   import { matchSorter } from 'match-sorter';
 
+  import { db } from '../stores/db';
+  import { isSpotlightOpen } from '../stores/common';
+
+  import { browser } from '$app/environment';
+  const API_ENDPOINT = import.meta.env.VITE_BACKEND_URL;
+
   let query = '';
 
   let items = [
     {
-      title: 'Hit 1',
-      description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. '
+      title: ' Home',
+      description: 'Ir al home',
+      url: '/'
     },
     {
-      title: 'Hit 2',
-      description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. '
-    },
-    {
-      title: 'Hit 3',
-      description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. '
-    },
-    {
-      title: 'Hit 4',
-      description: 'Lorem ipsum dolor sit, amet consectetur adipisicing ale. '
+      title: 'Blog',
+      description: 'Ir a la página principal del blog',
+      url: '/blog?page=1'
     }
   ];
-  $: results = matchSorter(items, query, { keys: ['title', 'description'] });
+
+  onMount(async () => {
+    const data = await fetch(`${API_ENDPOINT}/posts`);
+    const posts = await data.json();
+    items = items.concat(
+      await posts.map((x) => {
+        return {
+          description: x.Description,
+          title: `Blog - ${x.Title}`,
+          url: `/blog/${x.id}/${x.Slug}`
+        };
+      })
+    );
+  });
+
+  $: results = matchSorter(items, query, { keys: ['title', 'description'] }).slice(0, 5);
 </script>
 
 <div class="text-white font-sans" style="background:url(repeated-square-dark.webp) fixed #171718">
@@ -41,26 +58,39 @@
 <SvelteSpotlight
   {results}
   bind:query
-  modalClass={'w-[600px] max-w-[95%] bg-black shadow-lg rounded-sm'}
+  modalClass={'w-[600px] max-w-[95%] bg-black shadow-lg rounded-lg'}
   headerClass={'py-3 px-10 border-b-2 border-slate-100 border-b-solid'}
   inputClass="focus:outline-none bg-black text-white"
+  contentClass="text-white font-bold"
+  resultsClass="bg-black mt-2"
   resultIdKey="title"
+  searchPlaceholder="Buscar en todo el sitio"
   cleanQueryOnClose={true}
+  isOpen={$isSpotlightOpen}
   on:select={(event) => {
-    // DO stuff
+    window.location.href = event.detail.url;
   }}
 >
   <div
     slot="result"
     let:selected
     let:result
-    class={'hover:bg-slate-100 cursor-pointer text-sm px-10 py-3 w-full' + selected
-      ? 'bg-slate-100'
-      : ''}
+    class={`hover:bg-light-black cursor-pointer text-md pt-4 pb-2 px-4 w-full ${
+      selected ? 'bg-light-black' : ''
+    }`}
   >
     {result.title}
-    <p class="text-white text-sm">{result.description}</p>
+    <p class="mb-4 text-dim-white text-sm font-normal">{result.description}</p>
   </div>
 
-  <div slot="noResults" class="px-10 py-3 text-white text-sm">No results...</div>
+  <div slot="noResults" class="px-10 py-3 text-white text-center text-sm">No results...</div>
+
+  <div
+    slot="footer"
+    class="flex flex-row p-4 space-x-2 text-dim-white font-mono
+  font-bold text-sm text-right w-full justify-end"
+  >
+    <div class="font-mono bg-light-black px-2 rounded-md">↓↑ Navegar</div>
+    <div class="font-mono bg-light-black px-2 rounded-md">↲ Seleccionar</div>
+  </div>
 </SvelteSpotlight>
