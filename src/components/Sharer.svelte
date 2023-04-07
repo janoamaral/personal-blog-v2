57250e5a251de8e15567f3087573d4f683fc41ec
Alejandro Amaral <aamaral@logico.com.ar>
2023-04-07 16:42:00 -0300

    Ready to go

--- src/components/Sharer.svelte
+++ src/components/Sharer.svelte
@@ -51,9 +51,9 @@
 
 <div class={`flex items-center justify-center ${$isSharerOpen ? 'block' : 'hidden'}`}>
   <div class="fixed inset-0 z-50 flex items-center justify-center z-60">
-    <div class="relative bg-light-black p-6 rounded-lg max-w-md z-10">
+    <div class="relative bg-light-black p-6 rounded-lg max-w-md z-10 shadow-2xl">
       <button
-        class="absolute top-0 right-0 m-4 text-white bg-black/50 hover:bg-black/90 rounded-full p-2"
+        class="absolute top-0 right-0 m-4 text-white bg-black/40 hover:bg-black/90 rounded-full p-2"
         on:click={closeSharer}
         aria-label="Close sharer"
       >
@@ -73,18 +73,18 @@
         </svg>
       </button>
       <h2 class="text-2xl font-bold mb-2">Compartir</h2>
-      <div class="flex items-center mt-4 mb-6">
+      <div class="flex items-center mt-4 mb-6 bg-black rounded-md p-2 py-3">
         <input
-          class="text-black bg-gray-200 truncate rounded w-64 px-2 py-1 mr-2"
+          class="text-white bg-black truncate rounded w-full px-2 py-1 mr-2"
           type="text"
           readonly
           value={url}
         />
         <button
-          class="bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded"
+          class="bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-full"
           on:click={copyToClipboard}
         >
-          Copy
+          Copiar
         </button>
       </div>
       <div class="flex justify-between">
@@ -95,25 +95,25 @@
           <Icon type="whatsapp" gradientFrom="0,0,0,0" gradientTo="0,0,0,0" />
         </button>
         <button
-          class="w-1/6 h-16 pt-1 bg-blue-500 hover:bg-green-600 text-white font-bold rounded-full"
+          class="w-1/6 h-16 pt-1 bg-blue-500 text-white font-bold rounded-full"
           on:click={shareOnTwitter}
         >
           <Icon type="twitter" gradientFrom="0,0,0,0" gradientTo="0,0,0,0" />
         </button>
         <button
-          class="w-1/6 h-16 pt-1 pl-1 pr-8 bg-blue-800 hover:bg-green-600 text-white font-bold rounded-full"
+          class="w-1/6 h-16 pt-1 pl-1 pr-8 bg-blue-800  text-white font-bold rounded-full"
           on:click={shareOnFacebook}
         >
           <Icon type="facebook" gradientFrom="0,0,0,0" gradientTo="0,0,0,0" />
         </button>
         <button
-          class="w-1/6 h-16 pt-1 pl-1 pr-9 bg-blue-700 hover:bg-green-600 text-white font-bold rounded-full"
+          class="w-1/6 h-16 pt-1 pl-1 pr-9 bg-blue-700  text-white font-bold rounded-full"
           on:click={shareOnLinkedIn}
         >
           <Icon type="linkedin" gradientFrom="0,0,0,0" gradientTo="0,0,0,0" />
         </button>
         <button
-          class="w-1/6 h-16 pt-1 pl-1 pr-9 bg-accent-violet hover:bg-green-600 text-white font-bold rounded-full"
+          class="w-1/6 h-16 pt-1 pl-1 pr-9 bg-accent-violet  text-white font-bold rounded-full"
           on:click={shareByEmail}
         >
           <Icon type="mail" gradientFrom="0,0,0,0" gradientTo="0,0,0,0" />
