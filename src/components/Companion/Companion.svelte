63b7fc5c1d377ddc35b99a6f632e4ea56d77c143
Alejandro Amaral <aamaral@logico.com.ar>
2023-04-14 23:46:08 -0300

    Added tooltips to Companion

--- src/components/Companion/Companion.svelte
+++ src/components/Companion/Companion.svelte
@@ -33,7 +33,7 @@
     <li
       on:click={toggleCollapse}
       aria-label="Toggle collapse menu"
-      class={`cursor-pointer text-light-blue ${$isCollapsed ? '-ml-2' : ''}`}
+      class={`relative group cursor-pointer text-light-blue ${$isCollapsed ? '-ml-2' : ''}`}
     >
       {#if $isCollapsed}
         <button class="-translate-x-2" aria-label="Colapsar toolbar">
@@ -54,8 +54,18 @@
           />
         </button>
       {/if}
+      <div
+        class="text-xs font-bold uppercase inline-block absolute w-auto h-auto
+        p-2 top-0 left-0 text-white
+        bg-black/70 backdrop-blur-sm border border-light-black rounded-lg
+        -translate-x-2 translate-y-2
+         transition-all duration-200 ease-in-out opacity-0 none
+        group-hover:-translate-x-14 group-hover:opacity-100 group-hover:block z-0"
+      >
+        {$isCollapsed ? 'show' : 'hide'}
+      </div>
     </li>
-    <li class={`${$isCollapsed ? 'hidden' : 'block'}`}>
+    <li class={`relative group ${$isCollapsed ? 'hidden' : 'block'}`}>
       <a
         href="/"
         class="inline-block rounded-xl hover:text-accent-blue hover:bg-accent-blue/20"
@@ -63,8 +73,17 @@
       >
         <Icon size={6} type="home" gradientFrom="173, 182, 255, 0" gradientTo="174, 109, 255, 0" />
       </a>
+      <div
+        class="text-xs font-bold uppercase inline-block absolute w-auto h-auto bg-light-black p-2 top-0 left-0
+        bg-black/70 backdrop-blur-sm border border-light-black rounded-lg text-white
+        -translate-x-2 translate-y-2
+         transition-all duration-200 ease-in-out opacity-0 none
+        group-hover:-translate-x-16 group-hover:opacity-100 group-hover:block"
+      >
+        Home
+      </div>
     </li>
-    <li class={`${$isCollapsed ? 'hidden' : 'block'}`}>
+    <li class={`relative group ${$isCollapsed ? 'hidden' : 'block'}`}>
       <a
         href="/blog?page=1"
         aria-label="Blog"
@@ -73,24 +92,52 @@
       >
         <Icon size={6} type="edit" gradientFrom="173, 182, 255, 0" gradientTo="174, 109, 255, 0" />
       </a>
+      <div
+        class="text-xs font-bold uppercase inline-block absolute w-auto h-auto bg-light-black p-2 top-0 left-0
+        bg-black/70 backdrop-blur-sm border border-light-black rounded-lg text-white
+        -translate-x-2 translate-y-2
+         transition-all duration-200 ease-in-out opacity-0 none
+        group-hover:-translate-x-16 group-hover:opacity-100 group-hover:block"
+      >
+        Blog
+      </div>
     </li>
-    <li class={`${$isCollapsed ? 'hidden' : 'block'}`} on:click={openSpotlight}>
+    <li class={`relative group ${$isCollapsed ? 'hidden' : 'block'}`} on:click={openSpotlight}>
       <button
         aria-label="Abrir buscador"
         class="inline-block rounded-xl hover:text-accent-green
-      hover:bg-accent-green/20 pb-0 cursor-pointer"
+      hover:bg-accent-green/20 pb-0 cursor-pointer z-10"
       >
         <Icon size={6} type="search" gradientFrom="0, 255, 72, 0" gradientTo="0, 255, 72, 0" />
       </button>
+      <div
+        class="text-xs font-bold uppercase inline-block absolute w-auto h-auto bg-light-black p-2 top-0 left-0
+        bg-black/70 backdrop-blur-sm border border-light-black rounded-lg text-white
+        -translate-x-2 translate-y-2
+         transition-all duration-200 ease-in-out opacity-0 none
+        group-hover:-translate-x-20 group-hover:opacity-100 group-hover:block z-0"
+      >
+        search
+      </div>
     </li>
-    <li class={`${!showSharer || $isCollapsed ? 'hidden' : 'block'}`} on:click={openSharer}>
+    <li class={`relative group ${!showSharer || $isCollapsed ? 'hidden' : 'block'}`} on:click={openSharer}>
       <button
         aria-label="Compartir"
         class="inline-block rounded-xl hover:text-accent-orange
-      hover:bg-accent-orange/20 pb-0 cursor-pointer"
+      hover:bg-accent-orange/20 pb-0 cursor-pointer z-10"
       >
         <Icon size={6} type="share" gradientFrom="128, 72, 255, 0" gradientTo="0, 72, 255, 0" />
       </button>
+      <div
+        class="text-xs font-bold uppercase inline-block absolute w-auto h-auto
+        bg-light-black p-2 top-0 -left-1
+        bg-black/70 backdrop-blur-sm border border-light-black rounded-lg text-white
+        -translate-x-2 translate-y-2
+         transition-all duration-200 ease-in-out opacity-0 none
+        group-hover:-translate-x-16 group-hover:opacity-100 group-hover:block z-0"
+      >
+        share
+      </div>
     </li>
   </ul>
 </toolbar>
